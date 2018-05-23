8c2551c5ce1386b52688b33041d407c2a19de02e
Damian Muca <damian_muca@epam.com>
2018-05-23 16:16:51 +0200

    Create loop to prooced with Hanoi states

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,11 @@ public class Main {
     public static void main(String[] args) {
         RollersStackGenerator rollersStackGenerator = new RollersStackGenerator();
         rollersStackGenerator.setStackSize(3);
-
-        HanoiGame hanoiGame = new HanoiGame();
-        //hanoiGame.setUpNewGame(RollerStackGenerator.RollersStackGenerator.generateAndReturn(3));
+        HanoiGame hanoiGame = new HanoiGame(rollersStackGenerator.generate(), System.out::println);
+        
+        while(hanoiGame.notFinished()){
+            hanoiGame.nextState();
+            hanoiGame.printState();
+        }
     }
 }
--- src/main/java/Pillars/GamePillars.java
+++ src/main/java/Pillars/GamePillars.java
@@ -1,20 +1,21 @@
 package Pillars;
-
-import RollerStackGenerator.RollersStackGenerator;
+import RollerStackGenerator.Rollers.Roller;
+import RollerStackGenerator.Rollers.WrongRollerStack;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class GamePillars {
     private List<Pillar> pillars = new ArrayList<>(3);
 
-    public GamePillars(RollersStackGenerator rollersStackGenerator) {
-        initPillars();
-        //pillars.get(0).addRoller(rollersStackGenerator.generate());
-    }
-
-    private void initPillars() {
-        pillars.add(new Pillar());
+    public GamePillars(Stack<Roller> rollersStackGenerator) {
+        try {
+            pillars.add(new Pillar(rollersStackGenerator));
+        } catch (WrongRollerStack wrongRollerStack) {
+            wrongRollerStack.printStackTrace();
+            System.err.println(wrongRollerStack.getMessage());
+        }
         pillars.add(new Pillar());
         pillars.add(new Pillar());
     }
