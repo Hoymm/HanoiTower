4980184c6953a9bad7e59c98736b486bbd308a16
Damian Muca <damian_muca@epam.com>
2018-05-29 16:26:27 +0200

    Fix pom.xml artifactId error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import states.HanoiStartState;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Please insert rollers value in range [3-20], other will not be accepted");
+        System.out.println("Please insert rollers value in range [3-20], other value will will not be accepted");
         int howManyRollers = 5;
 
         new Thread(() -> {
